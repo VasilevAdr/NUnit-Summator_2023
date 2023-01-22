6e4e85f55bce5b9ef3c8c2767c44908603061159
VasilevAdr <91788080+VasilevAdr@users.noreply.github.com>
2023-01-22 20:49:12 +0200

    Add project files.

---
+++ Summator_2023/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Summator_2023
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Summator_2023.Sum(new int[] {1,2,3}));
+        }
+    }
+}
